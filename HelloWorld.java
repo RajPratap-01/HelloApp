b1b272743a89884ce262f6ce28392feef482f0d5
Raj Pratap <raj@Rajs-MacBook-Air-2.local>
2026-03-25 13:14:38 +0530

    UC1: Display Hello World on console

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Raj 🚀 GitHub Repo is working!");
+        System.out.println("Hello World");
     }
 }
 
