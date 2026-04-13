e54808340e6ac88f0f27d33ee166909506c5e7f0
Raj Pratap <raj@Rajs-MacBook-Air-2.local>
2026-04-13 15:03:40 +0530

    UC5: Display Hello using enhanced for loop

---
+++ uc5.java
@@ -0,0 +1,14 @@
+public class uc5 {
+    public static void main(String[] args) {
+
+        // If no arguments → default message
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Enhanced for loop
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        }
+    }
+}
